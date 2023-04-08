caefe24bb611ac6f0c31d70dc96759ab166db63f
Brendan Cordova <cordovalegacy19@gmail.com>
2023-04-08 14:11:47 -0700

    More practice with JS oop leading up to using inheritence, extends, and super

---
+++ Ninja.js
@@ -0,0 +1,32 @@
+class Ninja{
+    constructor(name, health){
+        this.name = name
+        this.health = health
+        this.speed = 3
+        this.strength = 3
+    }
+
+    sayName(){
+        console.log(`Ninja's Name: ${this.name}`)
+        return this
+    }
+
+    showStats(){
+        console.log(`${this.name}'s Stats\nHealth: ${this.health}\nSpeed: ${this.speed}\nStrength: ${this.strength}`)
+        return this
+    }
+
+    drinkSake(boost){
+        this.health = this.health + boost
+        return this
+    }
+}
+
+
+const boost = 10
+ninja1 = new Ninja("Charles", 10)
+ninja1.drinkSake(boost).sayName().showStats()
+
+//You don't need to return this when you're just console logging and not actually returning a value...BUT
+//in order to chain two methods that are not returning values, those methods need to return this. Otherwise, you'll get a TypeError because undefined doesn't have those methods.
+// So, in this particular case, you need to modify the sayName() and showStats() methods to return this in order to chain them together.
